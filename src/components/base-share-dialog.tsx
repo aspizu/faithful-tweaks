2204c3dca6a68b8288a438f68fe18e60ab24c2d6
aspizu <aspizu@protonmail.com>
2025-05-16 22:08:50 +0530

    refactor: move sharing logic into own file

--- src/components/base-share-dialog.tsx
+++ src/components/base-share-dialog.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/dialog"
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
-import {createShareURL} from "@/lib/state"
+import {createShareURL} from "@/lib/share"
 import {toast} from "sonner"
 
 interface BaseShareDialogProps {
--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,5 +1,4 @@
-import {customTweaks} from "@/lib/tweaks/custom-tweak"
-import {tweaks, type PackID, type TweakID} from "@/lib/tweaks/tweak"
+import {type PackID, type TweakID} from "@/lib/tweaks/tweak"
 import {signal} from "@preact/signals-react"
 import {produce} from "immer"
 
@@ -23,65 +22,4 @@ export function setTweakSelection(id: TweakID, value: boolean) {
         }
         tweaks.sort()
     })
-}
-
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-
-function encodeIndex(index: number): string {
-    const place0 = index % chars.length
-    const place1 = Math.floor(index / chars.length) % chars.length
-    return chars[place1] + chars[place0]
-}
-
-function decodeIndex(encoded: string): number {
-    const place0 = chars.indexOf(encoded[1])
-    const place1 = chars.indexOf(encoded[0])
-    return place0 + place1 * chars.length
-}
-
-function encodeSelectedTweaks(): string {
-    return selectedTweaks.value
-        .map((tweak) => (tweaks[tweak] ? encodeIndex(tweaks[tweak].index) : null))
-        .filter(Boolean)
-        .join("")
-}
-
-function decodeSelectedTweaks(encoded: string): TweakID[] {
-    const selectedTweaks: TweakID[] = []
-    for (let i = 0; i < encoded.length; i += 2) {
-        const index = decodeIndex(encoded.slice(i, i + 2))
-        const tweakID = Object.values(tweaks).find((tweak) => tweak.index === index)
-        if (tweakID) {
-            selectedTweaks.push(tweakID.id)
-        }
-    }
-    return selectedTweaks
-}
-
-export function createShareURL({utmCampaign}: {utmCampaign: string}): string {
-    const url = new URL(window.location.href)
-    url.searchParams.set("pack", selectedPack.value)
-    url.searchParams.set("tweaks", encodeSelectedTweaks())
-    if (utmCampaign) {
-        url.searchParams.set("utm_campaign", utmCampaign)
-    }
-    for (const {createShareURL} of Object.values(customTweaks)) {
-        createShareURL(url)
-    }
-    return url.toString()
-}
-
-export function loadShareURL() {
-    const url = new URL(window.location.href)
-    const pack = url.searchParams.get("pack")
-    const tweaks = url.searchParams.get("tweaks")
-    if (pack) {
-        selectedPack.value = pack as PackID
-    }
-    if (tweaks) {
-        selectedTweaks.value = decodeSelectedTweaks(tweaks)
-    }
-    for (const {loadShareURL} of Object.values(customTweaks)) {
-        loadShareURL(url)
-    }
 }
