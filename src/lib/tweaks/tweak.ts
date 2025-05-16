3f53bee903e95464dadf44ecd2578d76e85b63b8
aspizu <aspizu@protonmail.com>
2025-05-16 22:33:04 +0530

    refactor: add manifests with better type

--- src/lib/tweaks/tweak.ts
+++ src/lib/tweaks/tweak.ts
@@ -1,9 +1,13 @@
 import _tweaks from "@/assets/tweaks.json"
-import {customTweaks} from "@/lib/tweaks/custom-tweak"
+import {customTweaks, Manifest} from "@/lib/tweaks/custom-tweak"
 
 export const tweaks = _tweaks as Record<TweakID, Tweak>
 
 export const categories = ["aesthetic", "retro", "gui"] as const
+
+export const manifests = Object.values(tweaks).concat(
+    Object.values(customTweaks).map((t) => t.manifest) as any,
+) as (Tweak | Manifest)[]
 
 export const packs = {
     x32: {name: "Faithful 32x"},
